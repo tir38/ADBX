0b1a1eb9f01e6fcc939e43067798d6cc4a9a70b7
Jason Atwood <jwatwood@gmail.com>
2021-07-16 23:47:05 -0400

    Add command to uninstall package

--- print_manual.rb
+++ print_manual.rb
@@ -1,9 +1,10 @@
 def print_manual
   puts "Usage: ax [command]"
   puts "commands: "
-  puts "   add_wifi [ssid] [password]:   adds wifi to network configs"
+  puts "   add_wifi [ssid] [password]:   add wifi to network configs"
   puts "   help:            print this help manual"
   puts "   list_packages:   list all installed packages"
   puts "   max_bright:      set display to max brightness"
   puts "   reboot:          reboot the device"
+  puts "   uninstall_package [package]: remove installed package"
 end
