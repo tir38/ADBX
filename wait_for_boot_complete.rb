65377b2f10bc0a1e0f9fb2c2cf6434a7825249b8
Jason Atwood <jwatwood@gmail.com>
2022-02-07 18:49:07 -0500

    Convert wait_for_boot_complete to ruby

---
+++ wait_for_boot_complete.rb
@@ -0,0 +1,23 @@
+require "open3"
+
+TIMEOUT = 60 # seconds
+WAIT_CYCLE = 1 # seconds
+def wait_for_boot_complete
+  Open3.capture2('adb wait-for-device')
+  x, _ = Open3.capture2('adb shell getprop sys.boot_completed')
+  count = 0
+  until x.start_with?("1")
+    print "."
+    if count > TIMEOUT / WAIT_CYCLE
+      puts "Timed out waiting for reboot"
+      return false
+    end
+
+    sleep WAIT_CYCLE
+    count = count + 1
+    x, _ = Open3.capture2('adb shell getprop sys.boot_completed')
+  end
+  true
+end
+
+
