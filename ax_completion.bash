e5afa5ac387a6460a0b6fc43670eb4a58c31cc37
Jason Atwood <jwatwood@gmail.com>
2022-02-23 23:02:44 -0500

    Add missing functions to auto complete

--- ax_completion.bash
+++ ax_completion.bash
@@ -4,7 +4,7 @@ _ax_completions()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="add_wifi reboot max_bright list_packages help permissions settings_app uninstall_package pull_apks"
+    opts="add_wifi launch_app layout_bounds list_packages help max_bright permissions pull_apks reboot settings_app uninstall_package "
 
     # only tab complete second word in command string
     if [[ ${#COMP_WORDS[@]}  == 2 ]] ; then
