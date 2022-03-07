73e8b227c72ef2abb3f3cb26000d9f8a08b74580
Jason Atwood <jwatwood@gmail.com>
2022-03-07 11:16:19 -0500

    Add example secondary tab completion

--- ax_completion.bash
+++ ax_completion.bash
@@ -4,12 +4,20 @@ _ax_completions()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="add_wifi launch_app layout_bounds list_packages help max_bright permissions pull_apks reboot screenshot, settings_app uninstall_package version_name"
+    primary_opts="add_wifi launch_app layout_bounds list_packages help max_bright permissions pull_apks reboot screenshot, settings_app uninstall_package version_name test"
+    test_opts="alpha beta gamma"
 
-    # only tab complete second word in command string
+    #remember: COMP_WORDS[0] is `ax`
+    # tab complete primary word in command string
     if [[ ${#COMP_WORDS[@]}  == 2 ]] ; then
-	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	    COMPREPLY=( $(compgen -W "${primary_opts}" -- ${cur}) )
     	return 0
+    fi
+
+    # example: tab complete secondary word IFF primary command is "test"
+    if [[ ${#COMP_WORDS[@]}  == 3  && ${COMP_WORDS[1]}  == "test" ]] ; then
+      COMPREPLY=( $(compgen -W "${test_opts}" -- ${cur}) )
+      return 0
     fi
 }
 complete -F _ax_completions ax
